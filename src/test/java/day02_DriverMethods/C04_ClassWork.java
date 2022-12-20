0c0881c9e5e03e3b81f33e48bb216672075ccd10
Meryem <munirebor@gmail.com>
2022-12-20 20:59:02 +0000

    20/12/22

--- src/test/java/day02_DriverMethods/C04_ClassWork.java
+++ src/test/java/day02_DriverMethods/C04_ClassWork.java
@@ -7,20 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class C04_ClassWork {
     public static void main(String[] args) {
         System.setProperty("Webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();//acilan browser'i tam ekran yap
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         //a.web sayfasına gidin. https://www.amazon.com/
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.amazon.com/");
         //Search(ara) “city bike”
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("city bike", Keys.ENTER);
         //Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
-        WebElement aramaSonucYazisi= driver.findElement(By.className("sg-col-inner"));
-        System.out.println(aramaSonucYazisi.getText());
+        List<WebElement> aramaSonucYazisi = driver.findElements(By.className("sg-col-inner"));
+        System.out.println("Arama Sonuc Yazisi = " + aramaSonucYazisi.get(0).getText());
+        // Arama sonuc sayisini konsola yazdiriniz
+        String[] sonucSayisi = aramaSonucYazisi.get(0).getText().split(" ");
+        System.out.println("Sonuc Sayisi = " + sonucSayisi[2]);
+        // Sonuc sayisini Lambda ile yazdiriniz
+        Arrays.stream(aramaSonucYazisi.get(0).getText().split(" ")).limit(3).skip(2).forEach(System.out::println);
         //Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+        List<WebElement> ilkWE = driver.findElements(By.className("s-image"));
+        ilkWE.get(0).click();
     }
 }
